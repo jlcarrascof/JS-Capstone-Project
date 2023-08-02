3f68d332ef660ab6189bb9f694392848aa455d53
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-08-02 15:59:05 +0100

    Display comments for a given item on the Comments pop-up

--- src/modules/eventHandlers.js
+++ src/modules/eventHandlers.js
@@ -1,25 +1,57 @@
 import displayDetailCard from './displayDetailCard.js';
+import { createComment, getItemComments } from './commentsApi.js';
 
-const addComment = (e, showsData) => {
-  const showsComment = document.getElementById('shows-comment');
-  showsComment.style.display = 'flex';
+const displayComments = async (commentId) => {
+  const listCommet = document.querySelector('.listComment');
+  const comments = await getItemComments(commentId);
+  listCommet.innerHTML = '';
 
-  const item = e.target.closest('.btn-comment');
-
-  const id = +item.parentElement.dataset.id;
-  const showDetail = showsData.filter((el) => el.id === id);
-  displayDetailCard(...showDetail);
+  comments.forEach((comment) => {
+    const li = document.createElement('li');
+    li.innerHTML = `Date: ${comment.creation_date} <strong>${comment.username}</strong>  : ${comment.comment} `;
+    listCommet.appendChild(li);
+  });
 };
 
 const initializeEventHandlers = (showsData) => {
   const btnComment = document.querySelectorAll('.btn-comment');
   const closeBtn = document.querySelector('.close-btn');
+  const addComment = document.querySelector('.form');
+  const nameInput = document.querySelector('.nameInput');
+  const commentInput = document.querySelector('.commentInput');
+  const numberCommet = document.querySelector('.numberCommet');
 
-  btnComment.forEach((btn) => btn.addEventListener('click', (e) => addComment(e, showsData)));
+  let commentId;
+
+  btnComment.forEach((btn) => btn.addEventListener('click', () => {
+    commentId = +btn.parentElement.dataset.id;
+    const showDetail = showsData.filter((el) => el.id === commentId);
+
+    // display Detail Card
+    displayDetailCard(...showDetail);
+
+    // Add comments counter
+    numberCommet.innerHTML = 'comment (5)';
+
+    // display comment
+    displayComments(commentId);
+  }));
+
+  addComment.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    // create Comment
+    await createComment(commentId, nameInput.value, commentInput.value);
+
+    // display comment
+    displayComments(commentId);
+
+    nameInput.value = '';
+    commentInput.value = '';
+  });
 
   closeBtn.addEventListener('click', () => {
-    const showsComment = document.getElementById('shows-comment');
-    showsComment.style.display = 'none';
+    document.getElementById('shows-comment').style.display = 'none';
   });
 };
 
